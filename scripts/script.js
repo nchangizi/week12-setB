d5d19083e74ab357c55c8b0f5629a16c15d66f43
Neda <nedachangizi@gmail.com>
2023-03-27 10:43:19 -0700

    change bg pink on click

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,17 @@ const buttonRef = document.querySelector("button");
 
 function alertUser() {
   alert("You clicked!!!!");
-//   buttonRef.removeEventListener("click", alertUser);
+  //   buttonRef.removeEventListener("click", alertUser);
 }
 
-buttonRef.addEventListener("click", alertUser,{once:true} );
+buttonRef.addEventListener("click", alertUser, { once: true });
+
+function changeBGPink() {
+  // document.body.style.backgroundColor= "pink"
+  document.body.classList.add("pinkBG");
+}
+
+buttonRef.addEventListener("click", changeBGPink);
+
+function changeText() {}
+buttonRef.addEventListener("click", changeText);
