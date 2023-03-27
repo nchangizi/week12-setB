70ed28ecd138f1fd74edf69f0382dd37d381ebf6
Neda <nedachangizi@gmail.com>
2023-03-27 11:10:52 -0700

    div with button

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,12 @@
-const buttonRef = document.querySelector("button");
+// const buttonRef = document.querySelector("button");
+// buttonRef.addEventListener("click", updateImage);
 
-function alertUser() {
-  alert("You clicked!!!!");
-  //   buttonRef.removeEventListener("click", alertUser);
-}
-
-buttonRef.addEventListener("click", alertUser, { once: true });
-
-function changeBGPink() {
-  // document.body.style.backgroundColor= "pink"
-  document.body.classList.add("pinkBG");
-}
-
-buttonRef.addEventListener("click", changeBGPink);
-
-function changeText() {
-  // if the text says "Click Me!" -> change it to "clicked"
-  // if it says "clicked!" -> change it back to "Click Me!"
-  if (buttonRef.textContent === "Click Me!") {
-    buttonRef.textContent = "Clicked!";
-  } else {
-    buttonRef.textContent = "Click Me!";
+const buttonContainer = document.querySelector(".button-container");
+// event parameter automatically receives event object
+function bgChange(event) {
+  //   console.log("buttons clicked ", event.target.tagName);
+  if (event.target.tagName === "BUTTON") {
+    event.target.classList.add("greenBG");
   }
 }
-buttonRef.addEventListener("click", changeText);
-
-function updateImage() {
-  const image = document.querySelector("#shoppingCart");
-  image.setAttribute("src", "images/shoppingCart.png");
-  image.setAttribute("alt", "shopping Cart");
-  image.setAttribute("width", 50);
-  image.setAttribute("height", 50);
-}
-buttonRef.addEventListener("click", updateImage);
+buttonContainer.addEventListener("mouseover", bgChange);
