d0222722e14b3def7dc89953a9aa2b511bbd14e4
Neda <nedachangizi@gmail.com>
2023-03-27 10:46:35 -0700

    change text

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,20 @@ buttonRef.addEventListener("click", alertUser, { once: true });
 
 function changeBGPink() {
   // document.body.style.backgroundColor= "pink"
-  document.body.classList.add("pinkBG");
+  document.body.classList.toggle("pinkBG");
 }
 
 buttonRef.addEventListener("click", changeBGPink);
 
-function changeText() {}
+function changeText() {
+  // if the text says "Click Me!" -> change it to "clicked"
+  // if it says "clicked!" -> change it back to "Click Me!"
+  if (buttonRef.textContent === "Click Me!") {
+    buttonRef.textContent = "Clicked!";
+  }
+  else{
+    buttonRef.textContent = "Click Me!";
+
+  }
+}
 buttonRef.addEventListener("click", changeText);
